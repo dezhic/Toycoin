6f6b02757da915faf562086f38321bd65a7b6490
Skineggzi <wongyunpui0404@gmail.com>
2023-03-23 19:30:44 +0800

    create Block object

---
+++ src/Block.java
@@ -0,0 +1,75 @@
+class Block {
+    private int index;
+    private String hash;
+    private String previousHash;
+    private int timestamp;
+    private String data;
+    private int difficulty; //The difficulty defines how many prefixing zeros the block hash must have, for the block to be valid.
+    private int nonce; // default: 0
+
+    public Block(int index, String hash, String previousHash, int timestamp, String data, int difficulty, int nonce) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.difficulty = difficulty;
+        this.nonce = nonce;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public int getNonce() {
+        return nonce;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setNonce(int nonce) {
+        this.nonce = nonce;
+    }
+}
