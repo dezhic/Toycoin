fae493a4f9f99723c2ffe3e41d34fd72d283a312
dezhic <chen.dz@live.com>
2023-04-03 17:40:40 +0800

    remove useless code

--- src/Main.java
+++ src/Main.java
@@ -38,33 +38,5 @@ public class Main {
         localServer.start();
         localClient.initialize();  // localClient needs to initialize AFTER localServer starts!
 
-        // TODO: Given a List of Txs List<Tx>, select max. 30 of them and build a MercleTree (So, we need a merkle tree data structure to contain Txs).
-        // ** Each block have its own Merkle tree
-        // List to store the transaction obj
-        List<Transaction> txMemPool = new ArrayList<>();
-        // TODO: some data should be add into txMemPool...
-        /** txMemPool.add(); */
-        // Also, we need to have the hash val convert by each transaction
-        List<String> txHashPool = new ArrayList<>();
-        // function: calculate the hash with SHA-256
-        for (Transaction tx : txMemPool) {
-            String txHash = ProofOfWork.calculateHash(tx.toString());
-            txHashPool.add(txHash); // put them into txPoolHash
-        }
-        // Then, put txHashPool into merkle tree function and generate
-        MerkleTree merkleTrees = new MerkleTree(txHashPool);
-//        merkleTrees.generateMerkleTreeRoot();
-        // get root => data
-//        System.out.println("root : " + merkleTrees.getRoot());
-
-
-//        if (System.getenv("PORT").equals("3888")) {
-//            blockchain.generateToAddress(5, "dummy_address");
-//        } else if (System.getenv("PORT").equals("3889")) {
-//            blockchain.sync();
-//        } else {
-//            blockchain.generateToAddress(3, "dummy_address");
-//            blockchain.sync();
-//        }
     }
 }
--- src/protocol/Payload.java
+++
@@ -1,6 +0,0 @@
-package protocol;
-
-import java.io.Externalizable;
-
-public abstract class Payload implements Externalizable {
-}
